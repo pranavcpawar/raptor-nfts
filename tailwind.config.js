870feca00c1d2019b5b53e2b7a8501f29c3b0573
pranavcpawar <pranavcpawar@outlook.com>
2023-06-25 23:40:24 +0530

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        epilogue: ['Epilogue', 'sans-serif'],
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
