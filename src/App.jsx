45d0cd550dde6c86f66c246d00edda4d5a54e959
pranavcpawar <pranavcpawar@outlook.com>
2023-06-29 02:19:49 +0530

    raw frontend structure ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { LowerSection, Navbar, Particle, UpperSection } from "./components";
 
 const App = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center">
+    <div className="min-h-screen flex justify-center items-center">
       <Particle />
-      <div className="z-10 relative w-[60%] top-8 p-4 mr-2 ml-2 mt-2 mb-1 rounded-box">
+      <div className="z-10 relative p-4 m-2">
         <Navbar />
       </div>
-      <div className="z-5 absolute w-[100vw] h-[95vh] top-4 p-4 mt-1 mr-2 ml-2 mb-2">
+      <div className="z-5 fixed w-[100vw] h-[100vh]">
         <UpperSection />
         <LowerSection />
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row p-2 m-1 h-[56px] bg-[#121212] bg-opacity-70 place-content-between items-center outline outline-2 outline-offset-0 outline-purple-800 rounded-[10px]">
-      <h1 className="font-poppins cursor-pointer font-bold">RaptorNFT</h1>
-      <button className="btn btn-sm lg:btn-wide rounded-[10px] font-poppins bg-gradient-to-t from-[#6d5593] to-[#9a175a] text-black cursor-pointer">Connect</button>
+    <div className="flex flex-col p-2 m-1 h-[160px] w-full bg-[#121212] bg-opacity-70 items-center justify-center space-y-4 outline outline-2 outline-offset-0 outline-black rounded-[20px] backdrop-blur-sm">
+      <h1 className="font-poppins text-xl cursor-pointer font-bold">RaptorNFT.</h1>
+      <div className="flex flex-col space-y-5">
+        <button className="btn btn-sm sm:btn-wide btn-outline font-poppins hover:text-black cursor-pointer bg-opacity-70 backdrop-blur-sm shadow-sm hover:border-black">Mint</button>
+        <button className="btn btn-sm sm:btn-wide btn-outline font-poppins hover:text-black cursor-pointer bg-opacity-70 backdrop-blur-sm shadow-sm hover:border-black">Connect</button>
+      </div>
     </div>
   );
 };
