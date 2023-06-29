e5710fbfaaf3fa029d1f3bce182541d05271cace
pranavcpawar <pranavcpawar@outlook.com>
2023-06-29 12:37:51 +0530

    animate gradient background

--- src/components/LowerSection.jsx
+++ src/components/LowerSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LowerSection = () => {
   return (
     <div className="flex justify-end h-[50%] bg-black bg-opacity-50">
-      <div className="w-[50%] bg-gradient-to-tl from-[#5c258d] to-[#4389a2]">
+      <div className="w-[50%] bg-gradient-to-br from-[#5c258d] to-[#4389a2] background-animate">
 
       </div>
     </div>
--- src/components/UpperSection.jsx
+++ src/components/UpperSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const UpperSection = () => {
   return (
     <div className="flex justify-start h-[50%] bg-black bg-opacity-50">
-      <div className="w-[50%] bg-gradient-to-br from-[#5c258d] to-[#4389a2]">
+      <div className="w-[50%] bg-gradient-to-tr from-[#5c258d] to-[#4389a2] background-animate">
 
       </div>
     </div>
