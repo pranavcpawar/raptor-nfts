ef6d78b4e39c1f21e0f0144b2986be15c1ea5b88
pranavcpawar <pranavcpawar@outlook.com>
2023-07-05 23:24:14 +0530

    page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { LowerSection, Navbar, Particle, Sidebar, UpperSection } from "./components";
+import { Route, Routes } from "react-router-dom";
+import { Artwork, Home, Mint } from "./pages";
 
 const App = () => {
   return (
-    <div className="min-h-screen grid justify-center">
+    <div className="min-h-screen grid justify-center items-center">
       <Particle />
       <div className="z-10 absolute p-4 m-2 top-48">
         <Sidebar />
@@ -15,6 +17,11 @@ const App = () => {
         <UpperSection />
         <LowerSection />
       </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/artwork" element={<Artwork />} />
+        <Route path="/mint" element={<Mint />} />
+      </Routes>
     </div>
   );
 };
