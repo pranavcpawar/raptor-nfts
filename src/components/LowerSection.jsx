12019d2fa9a54a7c8eeebb76d5892c21179b75da
pranavcpawar <pranavcpawar@outlook.com>
2023-07-06 21:22:28 +0530

    change background animation

--- src/components/LowerSection.jsx
+++ src/components/LowerSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LowerSection = () => {
   return (
     <div className="flex h-[50%] bg-black bg-opacity-50">
-      <div className="w-[50%] bg-gradient-to-tr from-[#5c258d] to-[#4389a2] background-animate">
+      <div className="w-[50%] lower-gradient">
 
       </div>
     </div>
--- src/components/UpperSection.jsx
+++ src/components/UpperSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const UpperSection = () => {
   return (
     <div className="flex justify-end h-[50%] bg-black bg-opacity-50">
-      <div className="w-[50%] bg-gradient-to-tr to-[#5c258d] from-[#4389a2] background-animate">
+      <div className="w-[50%] upper-gradient">
 
       </div>
     </div>
