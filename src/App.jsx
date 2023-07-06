f12c9686a563d7d0fba844d2faeec9199855ab83
pranavcpawar <pranavcpawar@outlook.com>
2023-07-06 21:25:18 +0530

    gradient layer moved to home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,10 @@ import { Artwork, Home, Mint } from "./pages";
 
 const App = () => {
   return (
-    <div className="min-h-screen grid justify-center items-center">
+    <div className="min-h-screen relative grid justify-center items-center">
       <Particle />
       <div className="z-10 absolute p-4 m-2 top-48">
         <Sidebar />
-      </div>
-      <div className="z-10 relative p-4 m-2">
-        <Navbar />
-      </div>
-      <div className="z-5 absolute w-[100vw] h-[100vh]">
-        <UpperSection />
-        <LowerSection />
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Navbar, UpperSection, LowerSection } from "../components";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="min-h-screen grid place-items-center">
+      <div className="z-10 absolute p-4 m-2">
+        <Navbar />
+      </div>
+      <div className="z-5 relative w-[100vw] h-[100vh]">
+        <UpperSection />
+        <LowerSection />
+      </div>
+    </div>
   );
 };
 
