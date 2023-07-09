4e3e5f9ea601cb9e7b3eba366a577d5e089e3e4e
pranavcpawar <pranavcpawar@outlook.com>
2023-07-09 23:36:20 +0530

    basic artwork design

--- src/pages/Artwork.jsx
+++ src/pages/Artwork.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const Artwork = () => {
   return (
-    <div>Artwork</div>
+    <div className="z-10 min-h-screen grid place-content-center">
+      <div className="flex flex-col sm:w-[65vw] sm:h-[90vh] w-[100vw] h-[100vh] shadow-[1px_2px_20px] shadow-[#ff33bb] bg-[#121212] bg-opacity-70 items-center justify-center rounded-box outline outline-black outline-2 outline-offset-0">
+        <div>
+          Artwork
+        </div>
+      </div>
+    </div>
   );
 };
 
