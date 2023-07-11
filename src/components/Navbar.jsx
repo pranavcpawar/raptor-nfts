1185f808936ef06585942ee2ff50cc0ca332c4ba
pranavcpawar <pranavcpawar@outlook.com>
2023-07-11 20:37:52 +0530

    link connectWallet with connect button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useMintDappContext } from "../../context";
 
 const Navbar = () => {
+
+  const { connectWallet } = useMintDappContext();
+
   return (
     <div className="flex flex-col p-2 m-1 sm:h-[160px] w-full bg-[#121212] bg-opacity-70 items-center justify-center space-y-4 outline outline-2 outline-offset-0 outline-black rounded-box backdrop-blur-sm">
       <h1 className="font-poppins text-xl hidden sm:flex cursor-pointer font-bold">RaptorNFT.</h1>
       <div className="flex flex-col space-y-5">
         <button className="btn btn-sm sm:btn-wide btn-outline font-poppins hover:text-black cursor-pointer bg-opacity-70 backdrop-blur-sm shadow-sm hover:border-black">Mint</button>
-        <button className="btn btn-sm sm:btn-wide btn-outline font-poppins hover:text-black cursor-pointer bg-opacity-70 backdrop-blur-sm shadow-sm hover:border-black">Connect</button>
+        <button className="btn btn-sm sm:btn-wide btn-outline font-poppins hover:text-black cursor-pointer bg-opacity-70 backdrop-blur-sm shadow-sm hover:border-black" onClick={connectWallet}>Connect</button>
       </div>
     </div>
   );
