626565f2b317e5af233a36fc521c00c0d35d8c99
pranavcpawar <pranavcpawar@outlook.com>
2023-07-12 22:42:23 +0530

    adds website name on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,13 @@ import { Navbar, UpperSection, LowerSection } from "../components";
 
 const Home = () => {
   return (
-    <div className="min-h-screen grid place-items-center">
+    <div className="min-h-screen grid place-items-center ">
+      <div className="min-h-screen z-10 absolute pt-5">
+        <h1 className="font-poppins text-5xl cursor-pointer font-bold">
+          <span>Raptors</span>
+          <span className="text-black">NFT.</span>
+        </h1>
+      </div>
       <div className="z-10 absolute p-4 m-2">
         <Navbar />
       </div>
