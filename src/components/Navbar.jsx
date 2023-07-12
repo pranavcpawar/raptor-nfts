d211a2c0bf9d6d6633f624d98aa067e370218f87
pranavcpawar <pranavcpawar@outlook.com>
2023-07-12 22:45:03 +0530

    modifies navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { useMintDappContext } from "../../context";
+import ConnectWallet from "./ConnectWallet";
 
 const Navbar = () => {
 
   const { connectWallet } = useMintDappContext();
 
   return (
-    <div className="flex flex-col p-2 m-1 sm:h-[160px] w-full bg-[#121212] bg-opacity-70 items-center justify-center space-y-4 outline outline-2 outline-offset-0 outline-black rounded-box backdrop-blur-sm">
-      <h1 className="font-poppins text-xl hidden sm:flex cursor-pointer font-bold">RaptorNFT.</h1>
-      <div className="flex flex-col space-y-5">
-        <button className="btn btn-sm sm:btn-wide btn-outline font-poppins hover:text-black cursor-pointer bg-opacity-70 backdrop-blur-sm shadow-sm hover:border-black">Mint</button>
-        <button className="btn btn-sm sm:btn-wide btn-outline font-poppins hover:text-black cursor-pointer bg-opacity-70 backdrop-blur-sm shadow-sm hover:border-black" onClick={connectWallet}>Connect</button>
+    <div className="flex flex-col p-2 m-1 w-[220px] h-[160px] bg-[rgba(0,0,0,0.25)] bg-opacity-70 place-content-center outline outline-2 outline-offset-0 outline-black rounded-box backdrop-blur-sm shadow-[1px_2px_20px]">
+      <div className="flex flex-col space-y-5 items-center justify-between">
+        <ConnectWallet />
+        <button className= "font-poppins text-md w-[160px] h-[48px] p-2 bg-[#161618] outline outline-2 outline-offset-0 outline-black rounded-box backdrop-blur-sm shadow-[1px_1px_20px] shadow-black hover:bg-[#202020] active:scale-110 font-semibold">Mint</button>
       </div>
     </div>
   );
