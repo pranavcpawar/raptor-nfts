3a303f35f5c0c5fb54256309d114346af02ed38a
pranavcpawar <pranavcpawar@outlook.com>
2023-07-14 23:05:09 +0530

    display user data on connect wallet button

--- src/components/ConnectWallet.jsx
+++ src/components/ConnectWallet.jsx
@@ -4,25 +4,26 @@ import { metamask } from "../assets";
 
 
 const truncate = (_walletAddress) => {
-  return _walletAddress.substring(0,6) + "..." + _walletAddress.slice(-4)
+  if (!_walletAddress) return ""
+  return _walletAddress.substring(0,6) + "..." + _walletAddress.slice(-4);
 }
 
 const ConnectWallet = () => {
 
-  const { isConnected, connectWallet } = useMintDappContext();
+  const { walletAddress, isConnected, connectWallet, walletBalance } = useMintDappContext();
 
-  const truncatedAddress = truncate("0xB1bD506fC9FD21299ABC790Bdf85A17AF74BCC6C");
+  const truncatedAddress = truncate(walletAddress);
 
   return (
     <div className="flex flex-col items-center">
       <button 
-        className= {`font-poppins text-md w-[160px] ${!isConnected ? "h-[48px]" : "h-[64px] w-[180px]"} p-2 bg-[#161618] outline outline-2 outline-offset-0 outline-black rounded-box backdrop-blur-sm shadow-[1px_1px_20px] shadow-black hover:bg-[#202020] active:scale-110 font-semibold`} 
+        className= {`font-poppins text-md w-[160px] ${!isConnected ? "h-[48px]" : "h-[64px] w-[180px]"} pt-2 pb-2 pr-[12px] pl-[12px] bg-[#161618] outline outline-2 outline-offset-0 outline-black rounded-box backdrop-blur-sm shadow-[1px_1px_20px] shadow-black hover:bg-[#202020] active:scale-110 font-semibold`} 
         onClick={() => { !isConnected ? connectWallet() : alert("modal open") }}>
         {isConnected ? (
-          <div className="flex flex-row items-center gap-2 justify-center">
+          <div className="flex flex-row items-center justify-between gap-2">
             <div className="flex flex-col items-start">
-              <h3 className="text-[#dededf] font-poppins text-sm font-medium">2 MATIC</h3>
-              <h3 className="text-xs font-poppins font-medium text-[#656565]">{truncatedAddress}</h3>
+              <h3 className="text-[#dededf] text-sm font-poppins font-medium">{walletBalance.toFixed(3)} MATIC</h3>
+              <h3 className="text-xs font-medium font-poppins text-[#656565]">{truncatedAddress}</h3>
             </div>
             <img src={metamask} alt="metamask" className="h-12 w-12" />
           </div>
