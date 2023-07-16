2fbc7524ccfaf9d25c7b65475c6f56c1949fd059
pranavcpawar <pranavcpawar@outlook.com>
2023-07-16 21:13:16 +0530

    link mintNft to mint button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import ConnectWallet from "./ConnectWallet";
 
 const Navbar = () => {
 
-  const { isConnected } = useMintDappContext();
+  const { isConnected, mintNft } = useMintDappContext();
   
   return (
     <div className="flex flex-col p-2 m-1 w-[220px] h-[160px] bg-[rgba(0,0,0,0.25)] bg-opacity-70 place-content-center outline outline-2 outline-offset-0 outline-black rounded-box backdrop-blur-sm shadow-[1px_2px_20px]">
       <div className="flex flex-col space-y-5 items-center justify-between">
         <ConnectWallet />
-        {isConnected && <button className= "font-poppins text-md w-[160px] h-[48px] p-2 bg-[#161618] outline outline-2 outline-offset-0 outline-black rounded-box backdrop-blur-sm shadow-[1px_1px_20px] shadow-black hover:bg-[#202020] active:scale-110 font-semibold">Mint</button>}
+        {isConnected && <button className= "font-poppins text-md w-[160px] h-[48px] p-2 bg-[#161618] outline outline-2 outline-offset-0 outline-black rounded-box backdrop-blur-sm shadow-[1px_1px_20px] shadow-black hover:bg-[#202020] active:scale-110 font-semibold" onClick={mintNft}>Mint</button>} 
       </div>
     </div>
   );
