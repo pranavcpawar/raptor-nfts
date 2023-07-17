1ddc35983b1494843130b130a5f3090f22a03cf7
pranavcpawar <pranavcpawar@outlook.com>
2023-07-17 22:25:28 +0530

    initialize loader and alert box in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { LowerSection, Particle, Sidebar, UpperSection } from "./components";
+import { AlertBox, Loader, LowerSection, Particle, Sidebar, UpperSection } from "./components";
 import { Route, Routes } from "react-router-dom";
 import { Artwork, Home, Mint } from "./pages";
 
 const App = () => {
   return (
-    <div className="min-h-screen relative grid justify-center items-center">
+    <div className="min-h-screen relative grid items-center justify-center">
       <Particle />
-      <div className="z-10 fixed p-4 m-2 top-48">
+      <div className="z-10 fixed p-4 m-2">
         <Sidebar />
       </div>
-      <div className="z-5 fixed w-[100vw] h-[100vh]">
+      <div className="fixed w-[100vw] h-[100vh]">
         <UpperSection />
         <LowerSection />
+      </div>
+      <div className="z-20 fixed">
+        <Loader />
+        <AlertBox />
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { default as UpperSection } from "./UpperSection";
 export { default as LowerSection } from "./LowerSection";
 export { default as Sidebar } from "./Sidebar";
 export { default as ConnectWallet } from "./ConnectWallet";
+export { default as Loader } from "./Loader";
+export { default as AlertBox } from "./AlertBox";
