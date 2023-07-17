f846769aee0db54cad6e5dc59e8851155684c366
pranavcpawar <pranavcpawar@outlook.com>
2023-07-17 22:22:47 +0530

    add loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { InfinitySpin } from "react-loader-spinner";
+import { useMintDappContext } from "../../context";
+
+const Loader = () => {
+
+  const { loader } = useMintDappContext();
+
+  return (
+    <div className={`fixed inset-0 min-h-screen bg-[rgba(0,0,0,0.3)] grid place-items-center bg-opacity-50 backdrop-blur-sm transform transition-transform duration-300 ${loader.isLoading ?"scale-100": "scale-0"} rounded-[5px]`}>
+      <div className="flex flex-col w-[300px] h-[160px] bg-[#0a0a0a] rounded-box outline outline-2 outline-offset-0 outline-black shadow-[1px_3px_20px] shadow-black items-center justify-center px-10 py-5">
+      <InfinitySpin width="200"/>
+        <div className="flex flex-col items-center justify-center space-y-3">
+          <h3 className="font-poppins text-xl text-[#d5d5d5]">Loading...</h3>
+          <p className="font-poppins text-xs text-[#d5d5d5]">{loader.msg}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
