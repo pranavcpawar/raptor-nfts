68c859aba2768b9b213ca3a5e00aa995c0232066
pranavcpawar <pranavcpawar@outlook.com>
2023-07-17 22:26:10 +0530

    refactor sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,18 +12,18 @@ const Sidebar = () => {
   },[])
 
   return (
-    <div className="bg-[#121212] bg-opacity-70 hidden backdrop-blur-sm outline outline-2 outline-offset-0 outline-black rounded-box w-[80px] sm:flex flex-col h-[320px] p-2">
-      <div className="flex flex-col items-center justify-center space-y-12 p-2 m-1">
+    <div className="bg-[#121212] bg-opacity-70 hidden backdrop-blur-sm outline outline-2 outline-offset-0 outline-black rounded-box w-[80px] sm:flex flex-col h-[240px] p-2">
+      <div className="flex flex-col items-center justify-center space-y-5 p-2 m-1">
         <Link to="/">
           <div className="w-[56px] h-[56px] flex items-center justify-center bg-[#212121] p-2 rounded-box cursor-pointer outline-black outline outline-2 outline-offset-0 hover:scale-105">
             <img className="w-full h-full" src={logo} alt="logo"/>
           </div>
         </Link>
-        <div className="flex flex-col items-center justify-center gap-5">
+        <div className="flex flex-col items-center justify-center gap-4">
           {navlinks.map((item) => (
             <Fragment key={item.name}>
               <div 
-                className={`cursor-pointer ${isActive && isActive === item.name && "text-[#ff33bb] bg-[rgba(255,51,187,0.2)]  bg-opacity-70 w-[70px] flex justify-center rounded-[5px] active:scale-110"}`} 
+                className={`cursor-pointer hover:scale-105 ${isActive && isActive === item.name && "text-[#ff33bb] bg-[rgba(255,51,187,0.2)]  bg-opacity-70 w-[70px] flex justify-center rounded-[5px] active:scale-110"}`} 
                 onClick={() => {
                   setIsActive(item.name);
                   navigate(item.link);
