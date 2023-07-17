23decfd45c4409e424a318594162c38feb0450ea
pranavcpawar <pranavcpawar@outlook.com>
2023-07-17 22:22:18 +0530

    modifies artwork page

--- src/pages/Artwork.jsx
+++ src/pages/Artwork.jsx
@@ -19,17 +19,17 @@ const Artwork = () => {
           <span className="text-black">work.</span>
         </h1>
       </div>
-      <div className="z-10 group fixed flex flex-col w-[450px] group-hover:h-[500px] bg-[rgba(0,0,0,0.25)] shadow-[1px_2px_20px] bg-opacity-70 rounded-box outline outline-black outline-2 outline-offset-0 backdrop-blur-sm">
+      <div className="z-10 group fixed flex flex-col w-[265px] group-hover:h-[320px] bg-[rgba(0,0,0,0.25)] shadow-[1px_2px_20px] bg-opacity-70 rounded-box outline outline-black outline-2 outline-offset-0 backdrop-blur-sm">
         <div className="carousel carousel-center pt-3 pb-3 pr-3 pl-3 space-x-5 rounded-box">
           {nfts.map((nft, i) => (
             <Fragment key={i}>
               <div className="carousel-item">
                 <div className="flex flex-col place-items-center space-y-3">
                   <a href={nft.url} target="_blank">
-                    <img src={nft.imageURL} className="h-[425px] w-[425px] outline-2 bg-[#0D0D0D] outline outline-black outline-offset-0 shadow-[1px_2px_20px] shadow-black rounded-box"/>
+                    <img src={nft.imageURL} className="h-[240px] w-[240px] outline-2 bg-[#0D0D0D] outline outline-black outline-offset-0 shadow-[1px_2px_20px] shadow-black rounded-box"/>
                   </a>
-                  <div className="group-hover:grid hidden bg-animate cursor-pointer w-[425px] h-[40px] place-content-center p-2 outline outline-2 outline-offset-0 outline-black rounded-box shadow-[1px_2px_20px] shadow-black">
-                    <h1 className="font-poppins text-xl font-semibold text-[black]">RaptorsNFT #{nft.id}</h1>
+                  <div className="group-hover:grid hidden bg-animate cursor-pointer w-[240px] h-[40px] place-content-center p-2 outline outline-2 outline-offset-0 outline-black rounded-[12px] shadow-[1px_2px_20px] shadow-black">
+                    <h1 className="font-poppins text-lg font-semibold text-[black]">RaptorsNFT #{nft.id}</h1>
                   </div>
                 </div>
               </div>
