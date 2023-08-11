99b28c8cd4743d1e662b00edd5ce08aa6600c6c5
pranavcpawar <pranavcpawar@outlook.com>
2023-08-11 14:59:45 +0530

    increase alertbox height

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -10,7 +10,7 @@ const AlertBox = () => {
 
   return (
     <div className={`fixed inset-0 min-h-screen place-items-center ${loader.isLoading && alertBox.isAlert == true ? " ": "bg-black bg-opacity-50 backdrop-blur-sm"} transform transition-transform duration-300 ${alertBox.isAlert ?"grid": "hidden"} rounded-[5px]`}>
-      <div className="flex flex-col w-[300px] h-[160px] bg-[#0a0a0a] rounded-box outline outline-2 outline-offset-0 outline-black shadow-[1px_3px_20px] shadow-black items-center justify-center px-10 py-5">
+      <div className="flex flex-col w-[300px] h-[176px] bg-[#0a0a0a] rounded-box outline outline-2 outline-offset-0 outline-black shadow-[1px_3px_20px] shadow-black items-center justify-center px-10 py-5">
         <div className="flex flex-col items-center justify-center space-y-3">
           {alertBox.color == "red" ? 
           <TbAlertSquareRoundedFilled size={48} color="#9d1020" />
