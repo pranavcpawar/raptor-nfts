931a20b788184adc34bdeec2831913f05d21dc63
pranavcpawar <pranavcpawar@outlook.com>
2023-08-11 14:54:40 +0530

    modal design fix

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -19,17 +19,17 @@ const Modal = ({ open, onClose, addr, address, balances }) => {
   };
 
   return (
-    <div onClick={handleClose} id="close" className="inset z-10 bg-black bg-opacity-25 backdrop-blur-sm w-[100vw] min-h-screen grid place-items-center">
-      <div className="w-[300px] p-4 h-[200px] bg-black text-white rounded-box outline outline-2 outline-offset-0 outline-[#212121]">
-        <div className="flex flex-row items-center justify-start gap-2">
-          <img src={metamask} alt="metamask" className="h-14 w-14" />
-          <div className="flex flex-col items-start">
+    <div onClick={handleClose} id="close" className="inset z-10 bg-black bg-opacity-70 backdrop-blur-sm w-[100vw] min-h-screen grid place-items-center">
+      <div className="w-[300px] p-4 h-[160px] bg-[#0a0a0a] text-white rounded-box outline outline-2 outline-offset-0 outline-[#212121] items-center justify-center flex flex-col gap-4 shadow-[1px_2px_20px] shadow-[#ff33bb]">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <img src={metamask} alt="metamask" className="h-16 w-16 cursor-pointer" />
+          <div className="flex flex-row items-center justify-center gap-2">
             <div className="flex place-items-center flex-row gap-2">
               <h3 className="text-[#dededf] text-md font-poppins font-medium">{address}</h3>
               <RxCopy color="#656565" onClick={handleCopy} className="cursor-pointer" />
               <ToastContainer theme="dark" />
             </div>
-            <h3 className="text-sm font-poppins text-[#656565]">{balances.toFixed(3)} MATIC</h3>
+            <h3 className="text-md font-poppins text-[#656565] cursor-pointer">{balances.toFixed(3)} MATIC</h3>
           </div>
         </div>
       </div>
