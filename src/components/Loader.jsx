d6a6de4223eaf4862a9128f8567251770081c1b8
pranavcpawar <pranavcpawar@outlook.com>
2023-08-11 15:00:08 +0530

    increase loader height

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -8,7 +8,7 @@ const Loader = () => {
 
   return (
     <div className={`fixed inset-0 min-h-screen bg-black place-items-center bg-opacity-50 backdrop-blur-sm transform transition-transform duration-300 ${loader.isLoading ? "grid": "hidden"} rounded-[5px]`}>
-      <div className="flex flex-col w-[300px] h-[160px] bg-[#0a0a0a] rounded-box outline outline-2 outline-offset-0 outline-black shadow-[1px_3px_20px] shadow-black items-center justify-center px-10 py-5">
+      <div className="flex flex-col w-[300px] h-[176px] bg-[#0a0a0a] rounded-box outline outline-2 outline-offset-0 outline-black shadow-[1px_3px_20px] shadow-black items-center justify-center px-10 py-5">
       <InfinitySpin width="200"/>
         <div className="flex flex-col items-center justify-center space-y-3">
           <h3 className="font-poppins text-xl text-[#d5d5d5]">Loading...</h3>
