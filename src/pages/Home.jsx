29e4710d168592a76e3d5e92c50d265a4ec44fa9
pranavcpawar <pranavcpawar@outlook.com>
2023-09-20 23:42:29 +0530

    combining two span tag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,7 @@ const Home = () => {
     <div className="min-h-screen grid place-items-center">
       <div className="min-h-screen z-10 fixed pt-5">
         <h1 className="font-poppins text-5xl cursor-pointer font-bold">
-          <span>Raptors</span>
-          <span className="text-black">NFT.</span>
+          <span>Raptors<span className="text-black">NFT.</span></span>
         </h1>
       </div>
       <div className="z-10 fixed p-4 m-2">
